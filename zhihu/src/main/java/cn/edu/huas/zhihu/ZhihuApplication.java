c5a1702928eb28efa0aa43de61e0b7250a08f716
ErnestHuJC <132913775+ErnestHuJC@users.noreply.github.com>
2023-06-15 16:58:16 +0800

    getAnswerInfos
    首页接受回答信息

--- zhihu/src/main/java/cn/edu/huas/zhihu/ZhihuApplication.java
+++ zhihu/src/main/java/cn/edu/huas/zhihu/ZhihuApplication.java
@@ -1,9 +1,12 @@
 package cn.edu.huas.zhihu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@MapperScan
 public class ZhihuApplication {
 
 	public static void main(String[] args) {
